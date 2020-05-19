6102c223043d1df4fbaf6340cd1dce191207b77f
aron <pachl.aron@gmail.com>
2020-05-19 14:47:45 +0200

    add product to cart api function

---
+++ src/main/webapp/static/js/addToCart.js
@@ -0,0 +1,34 @@
+function addToCart(buttons, cartListPlaces) {
+
+    for (let button of buttons) {
+        button.addEventListener('click', function (event) {
+            apiFetch(button.dataset.id);
+            raiseCartListLength(cartListPlaces);
+        })
+    }
+
+}
+
+function apiFetch(parameter) {
+    fetch(`/add-product?id=${parameter}`)
+        .then(response => response.json())
+        .then(data => data);
+}
+
+function raiseCartListLength(cartListPlace) {
+    if (cartListPlace.textContent === "") {
+        cartListPlace.textContent = "0";
+    }
+    let placeContent = parseInt(cartListPlace.textContent);
+    placeContent++;
+    cartListPlace.textContent = placeContent;
+    cartListPlace.classList.add("cart-items");
+}
+
+function main() {
+    let cartListPlace = document.querySelector(".cart-size");
+    let buttons = document.querySelectorAll(".add-button");
+    addToCart(buttons, cartListPlace);
+}
+
+main();
