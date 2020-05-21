8559130e7066c50372b56b280a3b93e7a3746346
aron <pachl.aron@gmail.com>
2020-05-21 15:41:38 +0200

    formated file

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -16,30 +16,25 @@ function payWithCreditCard(payContainer) {
     let creditCardButton = document.querySelector("#credit-card");
     creditCardButton.addEventListener('click', function (event) {
         clearContainer();
+        let date = new Date();
+        let thisYear = date.getFullYear();
         let selectOptions = `
                                     <form action="/" method="get">
                                     <label for="card-number">Card number:</label>
                                     <input type='text' id='card-number' required><br>
                                     <label for="card-holder">Bank:</label>
-                                    <input type='text' id='card-holder' required><br>`;
-
-        selectOptions += `<label for="month">Month:</label><select id='month'>`;
+                                    <input type='text' id='card-holder' required><br>
+                                    <label for="month">Month:</label><select id='month'>`;
         for (let month = 1; month <= 12; month++) {
-            selectOptions += `<option value="${month}">${month}</option>`;
+            selectOptions +=       `<option value="${month}">${month}</option>`;
         }
-        selectOptions +=
-            `</select><br><label for="year">Year:</label><select id="year">`;
-        let d = new Date();
-        let n = d.getFullYear();
-        for (let year = n; year < n + 6; year++) {
-            selectOptions += `<option value="${year}">${year}</option>`;
+        selectOptions +=           `</select><br><label for="year">Year:</label><select id="year">`;
+        for (let year = thisYear; year < thisYear + 6; year++) {
+            selectOptions +=       `<option value="${year}">${year}</option>`;
         }
-        selectOptions += `</select><br>`;
-
-
-        selectOptions += `
-        <label for="CVC">CVC:</label>
-                <input type='password' id='CVC' maxlength="3" required><br>
+        selectOptions += `          </select><br>
+                                    <label for="CVC">CVC:</label>
+                                    <input type='password' id='CVC' maxlength="3" required><br>
                                     <button type="submit">Pay</button> 
                                     </form>`;
         payContainer.innerHTML += selectOptions;
