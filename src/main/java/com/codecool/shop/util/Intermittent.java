996bb677390395e024d824128f4d6d0e6c555f03
Viktor Bocz <beviktor95@gmail.com>
2020-06-02 16:10:46 +0200

    New class for sending email

--- src/main/java/com/codecool/shop/util/Intermittent.java
+++ src/main/java/com/codecool/shop/util/Intermittent.java
@@ -1,4 +1,17 @@
 package com.codecool.shop.util;
 
+import com.codecool.shop.model.Order;
+
 public class Intermittent {
+    private static Order order;
+
+
+
+    public static Order getOrder() {
+        return order;
+    }
+
+    public static void setOrder(Order order) {
+        Intermittent.order = order;
+    }
 }
