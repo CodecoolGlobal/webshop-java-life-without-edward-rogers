42c94f4cf6355c891bda03edcc6ea9812fdc2247
Viktor Bocz <beviktor95@gmail.com>
2020-06-15 15:36:48 +0200

    New java class for users

--- src/main/java/com/codecool/shop/model/User.java
+++ src/main/java/com/codecool/shop/model/User.java
@@ -2,19 +2,60 @@ package com.codecool.shop.model;
 
 public class User {
     private String userName;
-    private String hashedPassword;
+    private byte[] hashedPassword;
     private String address;
     private int zip;
     private String country;
     private String city;
+    private String email;
+    private String phoneNumber;
     private Cart cart;
 
-    public User(String userName, String hashedPassword, String address, int zip, String country, String city){
+    public User(String userName, byte[] hashedPassword, String address, int zip, String country,
+                String city, String email, String phoneNumber){
         this.userName = userName;
         this.hashedPassword = hashedPassword;
         this.address = address;
         this.zip = zip;
         this.country = country;
         this.city = city;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public byte[] getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 }
