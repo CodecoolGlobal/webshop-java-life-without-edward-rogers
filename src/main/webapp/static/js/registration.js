efbb90cdaa99e6960135ca3c865fe5f6ccba9a71
Viktor Bocz <beviktor95@gmail.com>
2020-06-15 15:37:47 +0200

    Dynamic HTML for registration with JS

--- src/main/webapp/static/js/registration.js
+++ src/main/webapp/static/js/registration.js
@@ -1,10 +1,32 @@
-function validatePassword(){
-    let username = document.querySelector("#username");
-    let username = document.querySelector("#username");
+function validatePassword(password, passwordVer){
+    passwordVer.addEventListener("keyup", function (){
+        if (validate(password, passwordVer)){
+            if (passwordVer.classList.contains("redBorder")){
+                passwordVer.classList.remove("redBorder")
+            }
+            if (!(passwordVer.classList.contains("greenBorder"))){
+                passwordVer.classList.add("greenBorder");
+            }
+        } else {
+            if (passwordVer.classList.contains("greenBorder")){
+                passwordVer.classList.remove("greenBorder")
+            }
+            if (!(passwordVer.classList.contains("redBorder"))){
+                passwordVer.classList.add("redBorder");
+            }
+        }
+    });
+}
+
+function validate(password, passwordVer){
+    return password.textContent === passwordVer.textContent;
 }
 
 function main(){
-    validatePassword();
+    let password = document.querySelector("#password");
+    let passwordVer = document.querySelector("#passwordVer");
+
+    validatePassword(password, passwordVer);
 }
 
 main();
