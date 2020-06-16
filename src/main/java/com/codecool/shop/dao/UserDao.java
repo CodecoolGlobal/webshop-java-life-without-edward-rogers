878e27324dde0790def6137a08d17bb0cc1855e2
aron <pachl.aron@gmail.com>
2020-06-16 17:50:13 +0200

    interface for communication with user data

--- src/main/java/com/codecool/shop/dao/UserDao.java
+++ src/main/java/com/codecool/shop/dao/UserDao.java
@@ -4,9 +4,12 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.model.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserDao {
 
     void add(User user);
+    Map<String, String> getDataOfUser(String userName);
+    Map<String, String> getNameAndPassword(String userName);
 
 }
