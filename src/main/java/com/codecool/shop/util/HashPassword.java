9b2e7c8de21a309c1dc56c6852a7d5da453bb28f
aron <pachl.aron@gmail.com>
2020-06-16 17:49:01 +0200

    hash pw methods

--- src/main/java/com/codecool/shop/util/HashPassword.java
+++ src/main/java/com/codecool/shop/util/HashPassword.java
@@ -2,6 +2,7 @@ package com.codecool.shop.util;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
@@ -11,23 +12,44 @@ public class HashPassword {
     private static HashPassword instance;
     SecureRandom random;
 
-    private HashPassword(){
+    private HashPassword() {
         random = new SecureRandom();
     }
 
-    public static HashPassword getInstance(){
-        if(instance == null){
+    public static HashPassword getInstance() {
+        if (instance == null) {
             instance = new HashPassword();
         }
         return instance;
     }
 
-    public byte[] hash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        return factory.generateSecret(spec).getEncoded();
+    public String hash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String passwordToHash = "password";
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(passwordToHash.getBytes());
+            //Get the hash's bytes
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    public boolean verifyPassword(String loginPassword, String savedPassword) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        System.out.println(savedPassword);
+        System.out.println(hash(loginPassword));
+        return savedPassword.equals(hash(loginPassword));
     }
 }
